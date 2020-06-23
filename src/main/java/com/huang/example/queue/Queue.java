cd78ec1c03f2e0af4a17c397757de85b7e14ed5b
Createsequence <841396397@qq.com>
2020-06-23 22:04:38 +0800

    数据结构学习代码

---
+++ src/main/java/com/huang/example/queue/Queue.java
@@ -0,0 +1,90 @@
+package com.huang.example.queue;
+
+/**
+ * @Author：黄成兴
+ * @Date：2020-06-11 16:28
+ * @Description：用数组模拟队列
+ */
+public class Queue {
+
+    //队列最大长度
+    private int maxSize;
+    //存放数据的数组
+    private Object[] arr;
+    //头指针，指向队头的元素的前一个位置
+    private int front;
+    //尾指针，指向队尾的元素所在位置
+    private int rear;
+
+    //构造器
+    public Queue(int size) {
+        this.maxSize = size;
+        this.arr = new Object[maxSize];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    /**
+     * 判断队列是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        //当头指针和尾指针相等时队列为空
+        return rear == front;
+    }
+
+    /**
+     * 判断队列是否已满
+     * @return
+     */
+    public boolean isFull(){
+        //当尾指针等于maxSize-1时队列满
+        return rear == maxSize - 1;
+    }
+
+    /**
+     * 入队
+     * @param item 入队元素
+     * @return
+     */
+    public int addQueue(Object item) {
+        //判断队列是已满还是只是尾指针到头
+        if (isFull()) {
+            //如果队列已满
+            if (front == -1){
+                throw new RuntimeException("队列已满！");
+            }
+            //尾指针到头，触发数据迁移
+            System.out.println("尾指针到头，触发数据迁移！当前有元素" + (rear - front) + "个");
+            for (int i = front; i < rear; i++) {
+                arr[i - front] = arr[i];
+            }
+            System.out.println("原尾指针：" + rear + ";头指针：" + front);
+            //移动指针
+            rear -= front;
+            front = -1;
+            System.out.println("尾指针：" + rear + ";头指针：" + front);
+        }
+        //尾指针后移
+        rear++;
+        arr[rear] = item;
+        System.out.println("入队" + item);
+        return rear;
+    }
+
+    /**
+     * 出队
+     * @return
+     */
+    public Object quitQueue() {
+        //判断队列是否为空
+        if (isEmpty()) {
+            throw new RuntimeException("队列为空！");
+        }
+        //头指针后移
+        front++;
+        return arr[front];
+    }
+
+}
